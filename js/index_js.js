3a35f487ad2c81c9b803e5084f3cff694162473b
Subash Pradhan <suprowilde5@gmail.com>
2016-10-28 19:29:28 +0200

    added js file and css file to the folder

---
+++ js/index_js.js
@@ -0,0 +1,15 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("container").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "#000000";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById('container').style.marginLeft = "0";
+  
+}
+
+
+
