3648a20f72dfd0438d8c197799bbad233005e38c
Subash Pradhan <suprowilde5@gmail.com>
2016-10-31 21:58:01 +0100

    modified few things on all pages and added lot of images for gallery.html page

--- js/index_js.js
+++ js/index_js.js
@@ -1,3 +1,5 @@
+//ACCORDION //
+
 $(document).ready(function(){
 	var acc = document.getElementsByClassName('accordion');
 	var i;
@@ -10,9 +12,13 @@ $(document).ready(function(){
 	}
 });
 
+// GOOGLE TRANSLATE //
+
 function googleTranslateElementInit() {
 	new google.translate.TranslateElement({pageLanguage: 'en'}, 'google-translate');
 }
+
+// SIDE NAVIGATION //
 
 function openNav() {
 	document.getElementById("mySidenav").style.width = "250px";
@@ -25,29 +31,60 @@ function closeNav() {
 	
 }
 
+// GO UP ARROW //
 $(document).ready(function () {
+	$('.scroll-up').hide();
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.scroll-up').fadeIn();
-        } else {
-            $('.scroll-up').fadeOut();
-        }
-    });
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 200) {
+			$('.scroll-up').fadeIn('slow');
+		} else {
+			$('.scroll-up').fadeOut('slow');
+		}
+	});
 
-    $('.scroll-up').click(function () {
-        $("html, body").animate({
-            scrollTop: 0
-        }, 'fast');
-        return false;
-    });
+	$('.scroll-up').click(function () {
+		$("html, body").animate({
+			scrollTop: 0
+		}, 'fast');
+		return false;
+	});
 
 });
 
+// SLIDE SHOW //
 
+$(document).ready(function(){
+	var currentIndex = 0,
+	items = $('.myslides'),
+	itemAmt = items.length;
 
+	function cycleItems() {
+		
+		var item = $('.myslides').eq(currentIndex);
+		items.hide();
+		item.css('display', 'block');
+	}
 
+	$('.next').click(function() {
+		currentIndex += 1;
+		if (currentIndex > itemAmt - 1) {
+			currentIndex = 0;
+		}
+		cycleItems();
+	});
 
+	$('.prev').click(function() {
+		currentIndex -= 1;
+		if (currentIndex < 0) {
+			currentIndex = itemAmt - 1;
+		}
+		cycleItems();
+	});
+
+	cycleItems(0);
+
+});
 
 
 
